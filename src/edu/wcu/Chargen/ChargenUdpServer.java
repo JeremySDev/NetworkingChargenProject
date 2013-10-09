c965139dc75b021b2767baaca99fc0201fbf881b
jstilwell <jerstil93@gmail.com>
2013-10-09 19:15:29 -0400

    Jeremy Commit

--- src/edu/wcu/Chargen/ChargenUdpServer.java
+++ src/edu/wcu/Chargen/ChargenUdpServer.java
@@ -1,6 +1,11 @@
 package edu.wcu.Chargen;
 
 /**
+ * ChargenUdpServer is a class that extends AbstractChargenServer and provides a
+ * concrete implementation of the listen() method using UDP. The method
+ * repeatedly waits for a datagram from a client and responds appropriately.
+ * This will be a serial server.
+ *
  * @author Jeremy Stilwell
  * @author Alisha Hayman
  * @version 10/8/13.
