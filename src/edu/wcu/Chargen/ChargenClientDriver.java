82d1a7988d016ca1a262a1c7e4ede4a56c16598a
jstilwell <jerstil93@gmail.com>
2013-10-09 14:05:29 -0400

    update simple tags

--- src/edu/wcu/Chargen/ChargenClientDriver.java
+++ src/edu/wcu/Chargen/ChargenClientDriver.java
@@ -2,6 +2,7 @@ package edu.wcu.Chargen;
 
 /**
  * @author Jeremy Stilwell
+ * @author Alisha Hayman
  * @version 10/8/13.
  */
 public class ChargenClientDriver {
--- src/edu/wcu/Chargen/ChargenUdpServer.java
+++ src/edu/wcu/Chargen/ChargenUdpServer.java
@@ -1,7 +1,9 @@
 package edu.wcu.Chargen;
 
 /**
- * Created by jstilwell on 10/8/13.
+ * @author Jeremy Stilwell
+ * @author Alisha Hayman
+ * @version 10/8/13.
  */
 public class ChargenUdpServer {
 }
