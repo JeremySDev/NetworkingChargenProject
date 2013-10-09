b599625abbca16a6b35ac7fdfea7585f9ceb4a71
jstilwell <jerstil93@gmail.com>
2013-10-09 18:09:12 -0400

    Jeremy Commit

--- src/edu/wcu/Chargen/AbstractChargenServer.java
+++ src/edu/wcu/Chargen/AbstractChargenServer.java
@@ -1,6 +1,16 @@
 package edu.wcu.Chargen;
 
 /**
+ * AbstractChargenServer is an abstract class that implements the ChargenServer
+ * interface. This class includes attributes that are common to all Chargen
+ * server implementations, as well as protected methods that enable subclasses
+ * to access those attributes. For all constructors, if no
+ * ChargenCharacterSource is supplied, DefactoChargenCharacterSource is used by
+ * default. getCharacterSource() will return a ChargenCharacterSource – an
+ * object used to generated the character sequence produced by the server.
+ * changeSource(ChargenCharacterSource) will accept a new ChargenCharacterSource
+ * that will change the one used to generate characters.
+ *
  * @author Jeremy Stilwell
  * @author Alisha Hayman
  * @version 10/8/13.
