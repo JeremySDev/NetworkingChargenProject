23f32c075fd2f4790d6fcf57e04703970444c842
jstilwell <jerstil93@gmail.com>
2013-10-09 14:27:50 -0400

    update simple tags

--- src/edu/wcu/Chargen/AbstractChargenServer.java
+++ src/edu/wcu/Chargen/AbstractChargenServer.java
@@ -9,5 +9,20 @@ public abstract class AbstractChargenServer{
     private int port;
     private ChargenCharacterSource source;
 
+    public ChargenCharacterSource (){}
+    public ChargenCharacterSource (int port){}
+    public ChargenCharacterSource (ChargenCharacterSource source){}
+    public ChargenCharacterSource (int port, ChargenCharacterSource source){}
 
+    public int getPort () {
+        return port;
+    }
+
+    public ChargenCharacterSource getCharacterSource () {
+        return source;
+    }
+
+    public void changeSource (ChargenCharacterSource source) {
+        this.source = source;
+    }
 }
