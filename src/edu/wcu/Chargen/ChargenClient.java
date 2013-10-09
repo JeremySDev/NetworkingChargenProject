04fbb7e4258deea30c7e912307b6520dd561dcea
jstilwell <jerstil93@gmail.com>
2013-10-08 20:22:24 -0400

    first

---
+++ src/edu/wcu/Chargen/ChargenClient.java
@@ -0,0 +1,10 @@
+package edu.wcu.Chargen;
+
+/**
+ *
+ * @author Jeremy Stilwell
+ * @version 10/8/13.
+ */
+public interface ChargenClient {
+    public static void main(String[] args){}
+}
---
+++ src/edu/wcu/Chargen/Main.java
@@ -0,0 +1,8 @@
+package edu.wcu.Chargen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
