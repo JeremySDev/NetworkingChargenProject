fb516b57d6c90291bb652b9e934b2621e02b1d21
jstilwell <jerstil93@gmail.com>
2013-10-09 19:22:31 -0400

    Jeremy Commit

--- src/edu/wcu/Chargen/ChargenCharacterSource.java
+++ src/edu/wcu/Chargen/ChargenCharacterSource.java
@@ -1,6 +1,11 @@
 package edu.wcu.Chargen;
 
 /**
+ * The Chargen RFC does not specify the format of the character sequence
+ * produced by a server application. Therefore, servers may choose to vary the
+ * character sequence. The ChargenCharacterSource provides the abstract
+ * interface to a component that produces some character sequence for chargen.
+ *
  * @author Jeremy Stilwell
  * @author Alisha Hayman
  * @version 10/8/13.
