5ead1e2906242d47df0fa223ecdd9bbe0acca87a
jstilwell <jerstil93@gmail.com>
2013-10-10 14:12:41 -0400

    Jeremy Commit

--- src/edu/wcu/Chargen/ChargenServer.java
+++ src/edu/wcu/Chargen/ChargenServer.java
@@ -8,13 +8,13 @@ import java.net.DatagramSocket;
 /**
  * ChargenServer is the interface to a chargen server implementation. The
  * interface includes a method: listen(). Classes implementing this interface
- * will provide an implementation of this method. listen() will listen for
- * requests from clients.
+ * will provide an implementation of this method.
  *
  * @author Jeremy Stilwell
  * @author Alisha Hayman
  * @version 10/8/13
  */
 public interface ChargenServer {
+    /* listen will listen for requests from clients. */
     public void listen();
 }
--- src/edu/wcu/Chargen/ChargenServerDriver.java
+++ src/edu/wcu/Chargen/ChargenServerDriver.java
@@ -14,7 +14,6 @@ import java.net.DatagramSocket;
  * listen for incoming connections. If this parameter is not supplied, the
  * default value must be 19 (the “well-known” chargen port).
  *
- *
  * @author Jeremy Stilwell
  * @author Alisha Hayman
  * @version 10/8/13.
@@ -22,6 +21,40 @@ import java.net.DatagramSocket;
 public class ChargenServerDriver {
     public static void main(String[] args)
     {
+        /* default "well-known" chargen port number */
+        int portNum = 19;
 
+        /* Server type TCP of UDP*/
+        String serverType;
+
+        /* Not enough or too many cmd line args call usage */
+        if (args.length == 0 || args.length >= 3)
+        {
+            usage();
+        }
+
+        /* Only one arg then set the server type */
+        /*TODO: ask Kreahling if we should exit or try to get the right server type*/
+        if (args.length == 1)
+        {
+            serverType = args[0];
+            if (!serverType.equalsIgnoreCase("TCP") ||
+                    !serverType.equalsIgnoreCase("UDP"))
+            {
+                usage();
+            }
+        }
+
+        /* Two arguments set the port number as well */
+        if (args.length == 2)
+        {
+            portNum = Integer.decode(args[1]);
+        }
+    }
+
+    private static void usage() {
+        System.err.println(
+                "usage: java ChargenServerDriver <TCP or UDP> [port number]");
+        System.exit(1);
     }
 }
